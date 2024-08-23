9b371a872ebe269f07b30893854fc182932dd20a
ogolafreeman <jacktoneokwemba14@gmail.com>
2024-08-23 16:37:59 +0300

    Frontend

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './pages/Home/Home'
+import Navbar from './pages/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import ProjectDetails from './pages/ProjectDetails/ProjectDetails'
+import IssueDetails from './pages/IssueDetails/IssueDetails'
+import Subscription from './pages/Subscription/Subscription'
+
+function App() {
+
+
+  return (
+    <>
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/project/:id" element={<ProjectDetails/>} />
+      <Route path="/project/:projectId/issue/:issueId" element={<IssueDetails/>} />
+      <Route path="/upgrade_plan" element={<Subscription/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
